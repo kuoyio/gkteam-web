543d165b520516b9c0edc85a1536a20f764123ea
kuoyio <admin@kuoyio.cn>
2025-11-17 03:57:17 +0800

    refactor🔨: 修改登录页面内容

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -12,24 +12,26 @@ export default function LoginPage() {
   return (
     <>
       <div className="min-h-screen flex items-center justify-center px-4 bg-light-blue/20">
-        <div className="flex flex-col justify-center items-center shadow-2xl rounded-b-xl h-[320] w-[400] p-8">
-          <div className="flex justify-center items-center gap-x-1">
-            <Image
-              src={"/logo.png"}
-              height={24}
-              width={24}
-              alt={"logo"}
-              preview={false}
-            />
-            <h1
-              className={`text-2xl font-bold text-blue ${shrikhand.className}`}
-            >
-              GKTeam
-            </h1>
+        <div className="flex flex-col justify-center items-center shadow-2xl rounded-b-xl h-[350] w-[450] p-8 gap-y-4">
+          <div className="flex flex-col gap-y-1">
+            <div className="flex justify-center items-center gap-x-1">
+              <Image
+                src={"/logo.png"}
+                height={24}
+                width={24}
+                alt={"logo"}
+                preview={false}
+              />
+              <h1
+                className={`text-2xl font-bold text-blue ${shrikhand.className}`}
+              >
+                GKTeam
+              </h1>
+            </div>
+            <span className="text-sm text-dark-grey text-center">
+              考试不费脑，小分队陪跑
+            </span>
           </div>
-          <span className="text-sm text-dark-grey mb-10">
-            公考备考一站式服务平台
-          </span>
           <LoginForm />
         </div>
       </div>

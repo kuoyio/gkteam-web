1a15de9e1b8c4e51dcbadb52a711e0d9d9aaa12d
kuoyio <admin@kuoyio.cn>
2025-12-10 00:44:05 +0800

    refactor🔨: 修改底部文字

--- src/components/Layout/LayoutFooter.tsx
+++ src/components/Layout/LayoutFooter.tsx
@@ -11,7 +11,7 @@ const LayoutFooter = () => {
       className="flex flex-col items-center justify-center h-16 mt-auto layout-footer gap-y-1"
     >
       <span className="text-xs text-gray-500">
-        © {year} GKTeam. 保留所有权利
+        © {year} 公考小分队版权所有
       </span>
       <Link
         href="https://beian.miit.gov.cn/#/Integrated/index"

f5011b59b649bc186569b2f74b834c6f8773be44
kuoyio <admin@kuoyio.cn>
2025-12-23 15:43:06 +0800

    feat🚀: 调整用户页面布局宽度

--- src/app/(main)/user/layout.tsx
+++ src/app/(main)/user/layout.tsx
@@ -11,16 +11,18 @@ interface UserLayoutProps {
 
 export default function UserLayout({ children }: UserLayoutProps) {
   return (
-    <Row gutter={[16, 16]} className="py-4 px-4 sm:px-6">
-      <Col xs={24} sm={24} md={8} lg={6} xl={6}>
-        <Flex vertical gap="middle" className="w-full">
-          <UserInfoCard />
-          <UserMenu />
-        </Flex>
-      </Col>
-      <Col xs={24} sm={24} md={16} lg={18} xl={18}>
-        <Card>{children}</Card>
-      </Col>
-    </Row>
+    <section className="max-w-[1600px] mx-auto">
+      <Row gutter={[16, 16]} className="p-4">
+        <Col xs={24} sm={24} md={8} lg={6} xl={6}>
+          <Flex vertical gap="middle" className="w-full">
+            <UserInfoCard />
+            <UserMenu />
+          </Flex>
+        </Col>
+        <Col xs={24} sm={24} md={16} lg={18} xl={18}>
+          <Card>{children}</Card>
+        </Col>
+      </Row>
+    </section>
   );
 }

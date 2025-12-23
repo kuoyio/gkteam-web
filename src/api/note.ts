d7fdb0a1afce512af933959dee037c0a6ce3db95
kuoyio <admin@kuoyio.cn>
2025-12-23 16:24:58 +0800

    refactor🔨: 优化构建时重复请求getNoteTree接口

--- src/api/note.ts
+++ src/api/note.ts
@@ -1,6 +1,7 @@
+import { cache } from "react";
 import { NoteTreeResponse } from "@/src/type";
 import ssgClient from "@/src/lib/ssg-client";
 
-export const getNoteTree = async () => {
+export const getNoteTree = cache(async () => {
   return await ssgClient.get<NoteTreeResponse[]>("/notes");
-};
+});
